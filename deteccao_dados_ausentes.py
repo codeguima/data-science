df57c153baffb38d0475f151896f36fe8578f59a
Jhonny <111026602+jhonnycodedev@users.noreply.github.com>
2024-08-25 21:25:20 -0300

    ok

--- deteccao_dados_ausentes.py
+++ deteccao_dados_ausentes.py
@@ -5,11 +5,8 @@ df_customers = pd.read_csv('./datasets/churn_customers.csv')
 df_services = pd.read_csv('./datasets/churn_services.csv')
 df_contracts = pd.read_csv('./datasets/churn_contracts.csv')
 
-##imprmir dataframe
-#print(df_customers.info())
-
-#verificar tamanho do dataframe
-#print(len(df_customers))
+#transformar coluna totalcharges de string para float
+df_contracts.TotalCharges = pd.to_numeric(df_contracts.TotalCharges, errors='coerce')
 
 #rename usando Lista - Modificar todos os nomes de colunas
 df_customers.columns = ['IDCliente', 'Genero', 'Mais65anos', 'TemParceiro', 'TemDependentes']
@@ -17,29 +14,33 @@ df_customers.columns = ['IDCliente', 'Genero', 'Mais65anos', 'TemParceiro', 'Tem
 #renomear colunas
 df_services.rename(columns={'customerID': 'IDCliente'}, inplace=True)
 
-#print(df_services.info())
-
 #UNIFICAR DE CUSTOMERS, SERVICES E CONTRACTS, CRIANDO UM TERCEIRO DATAFRAME
 
 df_temp = df_customers.merge(df_services, on=['IDCliente'])
-
-#print(df_temp.info())
-
-#MOSTRAR AS 5 PRIMEIRAS LINHAS
-#print(df_temp.head(5))
-
 
 #UNIFICAR df_temp COM CONTRACTS, USANDO COLUNAS DE JUNÇÃO COM NOMES DISTINTOS
 
 df_churn_temp = df_temp.merge(df_contracts, left_on='IDCliente', right_on=['customerID'])
 
-print(df_churn_temp.info())
+#print(df_churn_temp.info())
 
 #UNIFICAR OS TRÊS DATAFRAMES AO MESMO TEMPO, COM COLUNAS COM NOMES DIFERENTES
 df_churn = df_customers.merge(df_services, on=['IDCliente']).merge(df_contracts, left_on=['IDCliente'], right_on=['customerID'])
 
-
 #retira a coluna do dataframe
 df_churn.drop(['customerID'], axis=1, inplace=True)
+#----------------------------------------------------------------------------------------------------#
 
-print(df_churn.info())
+#DETECÇÃO DE DADOS AUSENTES
+
+# detectar valores ausentes
+#print(df_churn.isna().sum())
+
+# detectar valores ausentes em uma coluna
+#print(df_churn.TotalCharges.isna().sum())
+
+#quantas linhas tem pelo menos 1 coluna com o valor ausente
+#print(df_churn[df_churn.isna().any(axis=1)])
+
+#quantas colunas tem pelo menos 1 valor ausente
+print(df_churn.isna().any(axis=0).sum())
--- lidando_com_valores_ausentes.py
+++ lidando_com_valores_ausentes.py
@@ -5,11 +5,8 @@ df_customers = pd.read_csv('./datasets/churn_customers.csv')
 df_services = pd.read_csv('./datasets/churn_services.csv')
 df_contracts = pd.read_csv('./datasets/churn_contracts.csv')
 
-##imprmir dataframe
-#print(df_customers.info())
-
-#verificar tamanho do dataframe
-#print(len(df_customers))
+#transformar coluna totalcharges de string para float
+df_contracts.TotalCharges = pd.to_numeric(df_contracts.TotalCharges, errors='coerce')
 
 #rename usando Lista - Modificar todos os nomes de colunas
 df_customers.columns = ['IDCliente', 'Genero', 'Mais65anos', 'TemParceiro', 'TemDependentes']
@@ -17,29 +14,27 @@ df_customers.columns = ['IDCliente', 'Genero', 'Mais65anos', 'TemParceiro', 'Tem
 #renomear colunas
 df_services.rename(columns={'customerID': 'IDCliente'}, inplace=True)
 
-#print(df_services.info())
-
 #UNIFICAR DE CUSTOMERS, SERVICES E CONTRACTS, CRIANDO UM TERCEIRO DATAFRAME
 
 df_temp = df_customers.merge(df_services, on=['IDCliente'])
-
-#print(df_temp.info())
-
-#MOSTRAR AS 5 PRIMEIRAS LINHAS
-#print(df_temp.head(5))
-
 
 #UNIFICAR df_temp COM CONTRACTS, USANDO COLUNAS DE JUNÇÃO COM NOMES DISTINTOS
 
 df_churn_temp = df_temp.merge(df_contracts, left_on='IDCliente', right_on=['customerID'])
 
-print(df_churn_temp.info())
+#print(df_churn_temp.info())
 
 #UNIFICAR OS TRÊS DATAFRAMES AO MESMO TEMPO, COM COLUNAS COM NOMES DIFERENTES
 df_churn = df_customers.merge(df_services, on=['IDCliente']).merge(df_contracts, left_on=['IDCliente'], right_on=['customerID'])
 
-
 #retira a coluna do dataframe
 df_churn.drop(['customerID'], axis=1, inplace=True)
+#----------------------------------------------------------------------------------------------------#
 
-print(df_churn.info())
+#REMOÇÃO DE VALORES AUSENTES
+
+#remover de forma direta e especifica a coluna que possui valores ausentes
+#print(df_churn.drop(columns=['TotalCharges'], axis=1))
+
+#remover colunas com valores ausentes
+df_churn.dropna(axis=1)
